be35bd8d29cd4434f28a4c5098255754cc39bf88
Jinunu99 <114578011+Jinunu99@users.noreply.github.com>
2023-07-04 20:34:38 +0900

    Update test.py

--- test_model/우진우/test.py
+++ test_model/우진우/test.py
@@ -1,9 +1,65 @@
-<<<<<<< HEAD
-print("dasfaf")
+import tensorflow as tf
+from tensorflow.python.keras.models import Model
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Dropout, concatenate, UpSampling2D
 
-print("aasdfasdfjasdk")
-print("dfafdasfdafa")
-=======
-print("우진우 파일")
-print("안녕")
->>>>>>> 7492e4ede8102f76dc9abedfe529c3518d85786a
+# U-Net 모델 구현
+def unet(input_shape=(256, 256, 3)):
+    inputs = Input(input_shape)
+    
+    # 인코더 부분
+    conv1 = Conv2D(64, 3, activation='relu', padding='same')(inputs)
+    conv1 = Conv2D(64, 3, activation='relu', padding='same')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    
+    conv2 = Conv2D(128, 3, activation='relu', padding='same')(pool1)
+    conv2 = Conv2D(128, 3, activation='relu', padding='same')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    
+    conv3 = Conv2D(256, 3, activation='relu', padding='same')(pool2)
+    conv3 = Conv2D(256, 3, activation='relu', padding='same')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    
+    conv4 = Conv2D(512, 3, activation='relu', padding='same')(pool3)
+    conv4 = Conv2D(512, 3, activation='relu', padding='same')(conv4)
+    drop4 = Dropout(0.5)(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+    
+    conv5 = Conv2D(1024, 3, activation='relu', padding='same')(pool4)
+    conv5 = Conv2D(1024, 3, activation='relu', padding='same')(conv5)
+    drop5 = Dropout(0.5)(conv5)
+    
+    # 디코더 부분
+    up6 = Conv2D(512, 2, activation='relu', padding='same')(UpSampling2D(size=(2, 2))(drop5))
+    merge6 = concatenate([drop4, up6], axis=3)
+    conv6 = Conv2D(512, 3, activation='relu', padding='same')(merge6)
+    conv6 = Conv2D(512, 3, activation='relu', padding='same')(conv6)
+    
+    up7 = Conv2D(256, 2, activation='relu', padding='same')(UpSampling2D(size=(2, 2))(conv6))
+    merge7 = concatenate([conv3, up7], axis=3)
+    conv7 = Conv2D(256, 3, activation='relu', padding='same')(merge7)
+    conv7 = Conv2D(256, 3, activation='relu', padding='same')(conv7)
+    
+    up8 = Conv2D(128, 2, activation='relu', padding='same')(UpSampling2D(size=(2, 2))(conv7))
+    merge8 = concatenate([conv2, up8], axis=3)
+    conv8 = Conv2D(128, 3, activation='relu', padding='same')(merge8)
+    conv8 = Conv2D(128, 3, activation='relu', padding='same')(conv8)
+    
+    up9 = Conv2D(64, 2, activation='relu', padding='same')(UpSampling2D(size=(2, 2))(conv8))
+    merge9 = concatenate([conv1, up9], axis=3)
+    conv9 = Conv2D(64, 3, activation='relu', padding='same')(merge9)
+    conv9 = Conv2D(64, 3, activation='relu', padding='same')(conv9)
+    
+    # 최종 출력
+    outputs = Conv2D(1, 1, activation='sigmoid')(conv9)
+    
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
+
+# U-Net 모델 생성
+model = unet()
+
+# 모델 컴파일
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# 모델 요약 출력
+model.summary()
