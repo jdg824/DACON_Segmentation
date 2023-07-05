eb7dbfa58f396c71d39ac65949e9e02ab61c3674
Yoenkyu <130036932+Yoenkyu@users.noreply.github.com>
2023-07-05 16:55:26 +0900

    Update test.py

--- test_model/곽연규/test.py
+++ test_model/곽연규/test.py
@@ -4,56 +4,6 @@ from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Dropout, concat
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 # U-Net 모델 정의
-# def unet(input_shape):
-#     # 인코더 부분
-#     inputs = Input(input_shape)
-#     conv1 = Conv2D(64, 3, activation='relu', padding='same')(inputs)
-#     conv1 = Conv2D(64, 3, activation='relu', padding='same')(conv1)
-#     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-
-#     conv2 = Conv2D(128, 3, activation='relu', padding='same')(pool1)
-#     conv2 = Conv2D(128, 3, activation='relu', padding='same')(conv2)
-#     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-
-#     conv3 = Conv2D(256, 3, activation='relu', padding='same')(pool2)
-#     conv3 = Conv2D(256, 3, activation='relu', padding='same')(conv3)
-#     pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
-
-#     conv4 = Conv2D(512, 3, activation='relu', padding='same')(pool3)
-#     conv4 = Conv2D(512, 3, activation='relu', padding='same')(conv4)
-#     drop4 = Dropout(0.5)(conv4)
-#     pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
-
-#     # 디코더 부분
-#     conv5 = Conv2D(1024, 3, activation='relu', padding='same')(pool4)
-#     conv5 = Conv2D(1024, 3, activation='relu', padding='same')(conv5)
-#     drop5 = Dropout(0.5)(conv5)
-
-#     up6 = Conv2DTranspose(512, 2, strides=(2, 2), padding='same')(drop5)
-#     up6 = concatenate([up6, drop4])
-#     conv6 = Conv2D(512, 3, activation='relu', padding='same')(up6)
-#     conv6 = Conv2D(512, 3, activation='relu', padding='same')(conv6)
-
-#     up7 = Conv2DTranspose(256, 2, strides=(2, 2), padding='same')(conv6)
-#     up7 = concatenate([up7, conv3])
-#     conv7 = Conv2D(256, 3, activation='relu', padding='same')(up7)
-#     conv7 = Conv2D(256, 3, activation='relu', padding='same')(conv7)
-
-#     up8 = Conv2DTranspose(128, 2, strides=(2, 2), padding='same')(conv7)
-#     up8 = concatenate([up8, conv2])
-#     conv8 = Conv2D(128, 3, activation='relu', padding='same')(up8)
-#     conv8 = Conv2D(128, 3, activation='relu', padding='same')(conv8)
-
-#     up9 = Conv2DTranspose(64, 2, strides=(2, 2), padding='same')(conv8)
-#     up9 = concatenate([up9, conv1], axis=3)
-#     conv9 = Conv2D(64, 3, activation='relu', padding='same')(up9)
-#     conv9 = Conv2D(64, 3, activation='relu', padding='same')(conv9)
-
-#     outputs = Conv2D(1, 1, activation='sigmoid')(conv9)
-
-#     model = Model(inputs=inputs, outputs=outputs)
-#     return model
-
 def unet(input_shape):
     # 인코더 부분
     inputs = Input(input_shape)
@@ -65,7 +15,31 @@ def unet(input_shape):
     conv2 = Conv2D(128, 3, activation='relu', padding='same')(conv2)
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
-    up8 = Conv2DTranspose(128, 2, strides=(2, 2), padding='same')(pool2)
+    conv3 = Conv2D(256, 3, activation='relu', padding='same')(pool2)
+    conv3 = Conv2D(256, 3, activation='relu', padding='same')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    conv4 = Conv2D(512, 3, activation='relu', padding='same')(pool3)
+    conv4 = Conv2D(512, 3, activation='relu', padding='same')(conv4)
+    drop4 = Dropout(0.5)(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+
+    # 디코더 부분
+    conv5 = Conv2D(1024, 3, activation='relu', padding='same')(pool4)
+    conv5 = Conv2D(1024, 3, activation='relu', padding='same')(conv5)
+    drop5 = Dropout(0.5)(conv5)
+
+    up6 = Conv2DTranspose(512, 2, strides=(2, 2), padding='same')(drop5)
+    up6 = concatenate([up6, drop4])
+    conv6 = Conv2D(512, 3, activation='relu', padding='same')(up6)
+    conv6 = Conv2D(512, 3, activation='relu', padding='same')(conv6)
+
+    up7 = Conv2DTranspose(256, 2, strides=(2, 2), padding='same')(conv6)
+    up7 = concatenate([up7, conv3])
+    conv7 = Conv2D(256, 3, activation='relu', padding='same')(up7)
+    conv7 = Conv2D(256, 3, activation='relu', padding='same')(conv7)
+
+    up8 = Conv2DTranspose(128, 2, strides=(2, 2), padding='same')(conv7)
     up8 = concatenate([up8, conv2])
     conv8 = Conv2D(128, 3, activation='relu', padding='same')(up8)
     conv8 = Conv2D(128, 3, activation='relu', padding='same')(conv8)
