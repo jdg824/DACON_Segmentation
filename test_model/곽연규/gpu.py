f3270d39ab8ce01c171310b7ad34fbf10fa6e882
sunjuwhan <mathsun0011@gmail.com>
2023-07-05 20:58:58 +0900

    b

---
+++ test_model/곽연규/gpu.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+
+# Set up GPU configuration
+physical_devices = tf.config.list_physical_devices('GPU')
+if physical_devices:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+else:
+    print("No GPU available. Switching to CPU mode.")
--- test_model/곽연규/tensorflow_v.py
+++ test_model/곽연규/tensorflow_v.py
@@ -1,2 +1,5 @@
-from tensorflow.python.client import device_lib
-device_lib.list_local_devices()
+import  tensorflow as tf
+print(tf.__version__)
+
+# from tensorflow.python.client import device_lib
+# device_lib.list_local_devices()
