e5bc2c77f49da4fb025b22f46404630a9aaae97e
Jung DongGyu <80143957+jdg824@users.noreply.github.com>
2023-07-06 23:43:44 +0900

    U-net

--- test_model/정동규/Satelite_U-Net_Model_01.py
+++ test_model/정동규/Satelite_U-Net_Model_01.py
@@ -1,11 +1,52 @@
-import os
+import numpy as np
 import tensorflow as tf
+import glob
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Dropout, concatenate, Conv2DTranspose
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Dropout, UpSampling2D, concatenate
+from PIL import Image
+
+t_img_file = glob.glob("C:\\Users\\IT\\Desktop\\DACON\\open\\tra_img\\train_img\\*.png")
+t_mask_file = glob.glob("C:\\Users\\IT\\Desktop\\DACON\\open\\tra_ma\\train_mask\\*.png")
+v_img_file = glob.glob("C:\\Users\\IT\\Desktop\\DACON\\open\\val_im\\val_img\\*.png")
+v_mask_file = glob.glob("C:\\Users\\IT\\Desktop\\DACON\\open\\val_ma\\val_ask\\*.png")
+
+t_img=[]
+t_mask=[]
+v_img=[]
+v_mask=[]
+
+# 이미지 배열을 저장할 리스트 생성
+
+for file in t_img_file:
+    image = Image.open(file)  # 이미지 파일 읽기
+    image_array = np.array(image)  # NumPy 배열로 변환)
+    t_img.append(image_array)  # 리스트에 이미지 배열 추가
+    
+print(t_img.shape)   
+
+for file in t_mask_file:
+    image = Image.open(file)  # 이미지 파일 읽기
+    image_array = np.array(image)  # NumPy 배열로 변환)
+    t_mask.append(image_array)  # 리스트에 이미지 배열 추가
+
+print(t_mask.shape) 
+
+for file in v_img_file:
+    image = Image.open(file)  # 이미지 파일 읽기
+    image_array = np.array(image)  # NumPy 배열로 변환)
+    v_img.append(image_array)  # 리스트에 이미지 배열 추가
+
+print(v_img.shape) 
+
+for file in v_mask_file:
+    image = Image.open(file)  # 이미지 파일 읽기
+    image_array = np.array(image)  # NumPy 배열로 변환)
+    v_mask.append(image_array)  # 리스트에 이미지 배열 추가
+
+print(v_mask.shape) 
 
 # U-Net 모델 정의
-def unet(input_shape):
+def unet(input_shape, num_classes):
     # 인코더 부분
     inputs = Input(input_shape)
     conv1 = Conv2D(64, 3, activation='relu', padding='same')(inputs)
@@ -50,16 +91,16 @@ def unet(input_shape):
     conv9 = Conv2D(64, 3, activation='relu', padding='same')(up9)
     conv9 = Conv2D(64, 3, activation='relu', padding='same')(conv9)
 
-    outputs = Conv2D(1, 1, activation='sigmoid')(conv9)
+    outputs = Conv2D(num_classes, 1, activation='softmax')(conv9)
 
     model = Model(inputs=inputs, outputs=outputs)
     return model
 
-# 입력 이미지의 크기 지정
 input_shape = (1024, 1024, 3)
+num_classes = 2
 
 # U-Net 모델 생성
-model = unet(input_shape)
+model = unet(input_shape, num_classes)
 
 # 데이터셋 경로 지정
 train_images_dir = "C:\\Users\\IT\\Desktop\\DACON\\open\\tra_img"
@@ -67,47 +108,52 @@ train_masks_dir = "C:\\Users\\IT\\Desktop\\DACON\\open\\tra_ma"
 val_images_dir = "C:\\Users\\IT\\Desktop\\DACON\\open\\val_im"
 val_masks_dir = "C:\\Users\\IT\\Desktop\\DACON\\open\\val_ma"
 
+# 데이터 증강 생성기
 datagen = ImageDataGenerator(rescale=1./255)
 
 # 훈련 데이터셋 생성
-train_dataset = datagen.flow_from_directory(
+train_images = datagen.flow_from_directory(
     train_images_dir,
     target_size=input_shape[:2],
+    batch_size=16,
     class_mode=None,
     seed=42
 )
 
-train_masks_dataset = datagen.flow_from_directory(
+train_masks = datagen.flow_from_directory(
     train_masks_dir,
     target_size=input_shape[:2],
+    batch_size=16,
     class_mode=None,
     seed=42
 )
 
-train_generator = zip(train_dataset, train_masks_dataset)
+train_generator = zip(train_images, train_masks)
 
 # 검증 데이터셋 생성
-val_dataset = datagen.flow_from_directory(
+val_images = datagen.flow_from_directory(
     val_images_dir,
     target_size=input_shape[:2],
+    batch_size=16,
     class_mode=None,
     seed=42
 )
 
-val_masks_dataset = datagen.flow_from_directory(
+val_masks = datagen.flow_from_directory(
     val_masks_dir,
     target_size=input_shape[:2],
+    batch_size=16,
     class_mode=None,
     seed=42
 )
 
-val_generator = zip(val_dataset, val_masks_dataset)
+val_generator = zip(val_images, val_masks)
 
 # 모델 학습 설정
-model.compile(optimizer='adam', loss='binary_crossentropy')
+model.compile(optimizer=Adam(learning_rate=0.001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 # 모델 학습
-model.fit(train_generator, epochs=1, validation_data=val_generator, batch_size=16)
+model.fit(train_generator, epochs=10, validation_data=val_generator)
 
 # 학습된 모델 저장
-model.save_weights("C:\\Users\\IT\\Desktop\\DACON\\open\\model\\u_net_model")
+model.save("C:\\Users\\IT\\Desktop\\DACON\\open\\model\\u_net_model.h5")
