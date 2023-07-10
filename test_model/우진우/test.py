1db1abf95441ca40eda194beefff282bf274b965
Jinunu99 <114578011+Jinunu99@users.noreply.github.com>
2023-07-10 21:08:55 +0900

    2
    2

--- test_model/우진우/test.py
+++ test_model/우진우/test.py
@@ -1,56 +1,17 @@
 import os
 import numpy as np
 import cv2
-import tensorflow as tf
-from tensorflow.python.keras.models import Model
-from tensorflow.python.keras.layers import Input, Conv2D, MaxPooling2D, Dropout, concatenate, Conv2DTranspose
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.utils import to_categorical
+import glob
 
-# 데이터 경로 설정
-train_img_folder = "C:\\Users\\JW\\Downloads\\version\\train_img"
-train_mask_folder = "C:\\Users\\JW\\Downloads\\version\\train_mask"
-test_img_folder = "C:\\Users\\JW\\Downloads\\version\\test_img"
-
-def load_data(img_folder, mask_folder):
-    img_files = os.listdir(img_folder)
-    mask_files = os.listdir(mask_folder)
-    images = []
-    masks = []
-    
-    for file in img_files:
-        img_path = os.path.join(img_folder, file)
-        mask_path = os.path.join(mask_folder, file)
-        
-        if os.path.isfile(img_path) and os.path.isfile(mask_path) and file.endswith('.png'):
-            try:
-                img = cv2.imread(img_path)
-                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-                mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
-                
-                if img is not None and mask is not None:
-                    images.append(img)
-                    masks.append(mask)
-                else:
-                    print(f"Failed to read file: {file}")
-            except Exception as e:
-                print(f"Error reading file: {file}")
-                print(str(e))
-        else:
-            print(f"Skipping file: {file}")
-    
-    if len(images) == 0:
-        raise ValueError("No images found. Please check the image folder path.")
-    if len(masks) == 0:
-        raise ValueError("No masks found. Please check the mask folder path.")
-    
-    return np.array(images), np.array(masks)
-
-
-
-# U-Net 모델 정의
 def unet_model(input_shape):
     inputs = Input(input_shape)
 
-    # Contracting Path
+    # Encoder
     conv1 = Conv2D(64, 3, activation='relu', padding='same')(inputs)
     conv1 = Conv2D(64, 3, activation='relu', padding='same')(conv1)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
@@ -72,57 +33,80 @@ def unet_model(input_shape):
     conv5 = Conv2D(1024, 3, activation='relu', padding='same')(conv5)
     drop5 = Dropout(0.5)(conv5)
 
-    # Expanding Path
-    up6 = Conv2DTranspose(512, 2, strides=(2, 2), padding='same')(drop5)
+    # Decoder
+    up6 = Conv2D(512, 2, activation='relu', padding='same')(UpSampling2D(size=(2, 2))(drop5))
     merge6 = concatenate([drop4, up6], axis=3)
     conv6 = Conv2D(512, 3, activation='relu', padding='same')(merge6)
     conv6 = Conv2D(512, 3, activation='relu', padding='same')(conv6)
 
-    up7 = Conv2DTranspose(256, 2, strides=(2, 2), padding='same')(conv6)
+    up7 = Conv2D(256, 2, activation='relu', padding='same')(UpSampling2D(size=(2, 2))(conv6))
     merge7 = concatenate([conv3, up7], axis=3)
     conv7 = Conv2D(256, 3, activation='relu', padding='same')(merge7)
     conv7 = Conv2D(256, 3, activation='relu', padding='same')(conv7)
 
-    up8 = Conv2DTranspose(128, 2, strides=(2, 2), padding='same')(conv7)
+    up8 = Conv2D(128, 2, activation='relu', padding='same')(UpSampling2D(size=(2, 2))(conv7))
     merge8 = concatenate([conv2, up8], axis=3)
     conv8 = Conv2D(128, 3, activation='relu', padding='same')(merge8)
     conv8 = Conv2D(128, 3, activation='relu', padding='same')(conv8)
 
-    up9 = Conv2DTranspose(64, 2, strides=(2, 2), padding='same')(conv8)
+    up9 = Conv2D(64, 2, activation='relu', padding='same')(UpSampling2D(size=(2, 2))(conv8))
     merge9 = concatenate([conv1, up9], axis=3)
     conv9 = Conv2D(64, 3, activation='relu', padding='same')(merge9)
     conv9 = Conv2D(64, 3, activation='relu', padding='same')(conv9)
     conv9 = Conv2D(2, 3, activation='relu', padding='same')(conv9)
 
+    # Output
     outputs = Conv2D(1, 1, activation='sigmoid')(conv9)
 
     model = Model(inputs=inputs, outputs=outputs)
+    model.compile(optimizer=Adam(learning_rate=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
+
     return model
 
-# 데이터 로드
-train_images, train_masks = load_data(train_img_folder, train_mask_folder)
-test_images, _ = load_data(test_img_folder, train_mask_folder)
 
-# 데이터 전처리
-train_images = train_images / 255.0
-train_masks = train_masks / 255.0
+def load_data(image_folder, mask_folder):
+    image_files = sorted(glob.glob(os.path.join(image_folder, '*.png')))
+    mask_files = sorted(glob.glob(os.path.join(mask_folder, '*.png')))
+    images = []
+    masks = []
+
+    for img_file, mask_file in zip(image_files, mask_files):
+        image = cv2.imread(img_file, cv2.IMREAD_UNCHANGED)
+        mask = cv2.imread(mask_file, cv2.IMREAD_UNCHANGED)
+
+        if image is None or mask is None:
+            print(f"Failed to load image or mask: {img_file}, {mask_file}")
+            continue
+
+        if image.shape[:2] != mask.shape[:2]:
+            print(f"Image and mask sizes do not match: {img_file}, {mask_file}")
+            continue
+
+        images.append(image)
+        masks.append(mask)
+
+    masks = np.array(masks) / 255.0
+    images = np.array(images)
+
+    return images, masks
+
+
+
+# 데이터 로드
+train_image_folder = "C:\\Users\\IT\\Desktop\\ai\\train_img"
+train_mask_folder = "C:\\Users\\IT\\Desktop\\ai\\train_mask"
+test_image_folder = "C:\\Users\\IT\\Desktop\\ai\\test_img"
+
+train_images, train_masks = load_data(train_image_folder, train_mask_folder, crop_size=224)
+test_images, _ = load_data(test_image_folder, test_image_folder, crop_size=224)
+
 
 # U-Net 모델 생성
-input_shape = train_images[0].shape
+input_shape = (224, 224, 3)  # Updated input shape to match resized train images and masks
 model = unet_model(input_shape)
 
-# 모델 컴파일
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
 # 모델 학습
-model.fit(train_images, train_masks, batch_size=16, epochs=10)
+model.fit(train_images, train_masks, batch_size=3, epochs=2)  # Removed reshape step
 
-# 테스트 데이터 예측
-test_images = test_images / 255.0
-predictions = model.predict(test_images)
-
-# 결과 시각화
-for i, pred in enumerate(predictions):
-    pred = np.squeeze(pred)
-    pred = np.round(pred * 255).astype(np.uint8)
-    cv2.imwrite(f'prediction_{i}.png', pred)
+# 모델 저장
+model.save("./train_model")
